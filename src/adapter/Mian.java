7d5c6ffb2bc11c8bcb693fe6cc320b3ae739249f
poneding <poneding@gmail.com>
2026-04-24 00:17:08 +0800

    .

---
+++ src/adapter/Mian.java
@@ -0,0 +1,13 @@
+package adapter;
+
+/**
+ * Mian: 适配器模式函数入口
+ *
+ * @author poneding@gmail.com
+ * @date 2025/9/9 08:15
+ */
+public class Mian {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/adapter/Target.java
@@ -0,0 +1,8 @@
+package adapter;
+
+/**
+ * Target：适配器模式目标接口
+ */
+public interface Target {
+
+}
